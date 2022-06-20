65d453f1ebb0d92ba6a51c31729830fea16cb769
Leandro Arturi <lea.arturi@gmail.com>
2022-06-20 15:31:51 -0300

    Shop Layout

---
+++ components/layouts/ShopLayout.tsx
@@ -0,0 +1,46 @@
+import React, { FC, ReactNode } from 'react';
+import Head from 'next/head';
+
+interface Props {
+    children: ReactNode;
+    title: string;
+    pageDescription: string;
+    imageFullUrl?: string;
+}
+
+export const ShopLayout:FC<Props> = ({
+    children,
+    title,
+    pageDescription,
+    imageFullUrl,
+}) => {
+  return (
+    <>
+        <Head>
+            <title>{title}</title>
+            <meta name='description' content={pageDescription} />
+            <meta name='og:title' content={title} />
+            <meta name='og:description' content={pageDescription} />
+            {
+                imageFullUrl && <meta name='og:image' content={imageFullUrl} />
+            }
+        </Head>
+
+        <nav>
+
+        </nav>
+
+        <main style={{ 
+            margin: '80px auto',
+            maxWidth: '1440px',
+            padding: '0px 30px',
+        }}>
+            {children}
+        </main>
+
+        <footer>
+
+        </footer>
+    </>
+  )
+}
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,13 @@
+import { Typography } from '@mui/material';
 import type { NextPage } from 'next';
+import { ShopLayout } from '../components/layouts';
 
 const Home: NextPage = () => {
   return (
-    <div>
-      <h1>Teslo App</h1>
-    </div>
+    <ShopLayout title={'Teslo-Shop: Home'} pageDescription={'Encuentra los productos que buscas en Teslo-Shop!'}>
+      <Typography variant='h1' component='h1'>Home</Typography>
+      <Typography variant='h2' sx={{ mb: 1 }}>Todos los productos</Typography>
+    </ShopLayout>
   )
 }
 
