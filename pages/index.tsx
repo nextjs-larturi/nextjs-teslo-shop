c1314f60608266ac32981ce1e242539ddc66dcdd
Leandro Arturi <lea.arturi@gmail.com>
2022-06-21 14:27:34 -0300

    List Cards Index

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,12 +1,33 @@
-import { Typography } from '@mui/material';
 import type { NextPage } from 'next';
+import { Card, CardActionArea, CardMedia, Grid, Typography } from '@mui/material';
 import { ShopLayout } from '../components/layouts';
+import { initialData } from '../database/products';
 
 const Home: NextPage = () => {
   return (
     <ShopLayout title={'Teslo-Shop: Home'} pageDescription={'Encuentra los productos que buscas en Teslo-Shop!'}>
       <Typography variant='h1' component='h1'>Home</Typography>
       <Typography variant='h2' sx={{ mb: 1 }}>Todos los productos</Typography>
+
+      <Grid container spacing={4}>
+        {
+          initialData.products.map(product => (
+            <Grid item xs={6} sm={4} xl={3} key={product.slug}>
+              <Card>
+                <CardActionArea>
+                  <CardMedia
+                    component='img'
+                    image={`products/${product.images[0]}`}
+                    alt={product.title}
+                  >
+
+                  </CardMedia>
+                </CardActionArea>
+              </Card>
+            </Grid>
+          ))
+        }
+      </Grid>
     </ShopLayout>
   )
 }
