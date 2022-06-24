27ac4be10f7a07c37ebcf1073cdf1ed2d3b4a81e
Leandro Arturi <lea.arturi@gmail.com>
2022-06-24 16:44:55 -0300

    Login Page

---
+++ components/layouts/AuthLayout.tsx
@@ -0,0 +1,24 @@
+import { FC, ReactNode } from 'react';
+import Head from 'next/head';
+import { Box } from '@mui/system';
+
+interface Props {
+    children: ReactNode;
+    title: string;
+}
+
+export const AuthLayout: FC<Props> = ({ children, title }) => {
+  return (
+    <>  
+        <Head>
+            <title>{title}</title>
+        </Head>
+
+        <main>
+            <Box display='flex' justifyContent='center' alignItems='center' height='calc(100vh - 200px)'>
+                { children }
+            </Box>
+        </main>
+    </>
+  )
+}
