79bc1329b58c8765328e81173d43776a6f5450ed
Leandro Arturi <lea.arturi@gmail.com>
2022-06-25 19:33:11 -0300

    Seed DB

---
+++ database/db.ts
@@ -0,0 +1,40 @@
+import mongoose from 'mongoose';
+
+/**
+ * 0 = disconnected
+ * 1 = connected
+ * 2 = connecting
+ * 3 = disconnecting
+ */
+const mongoConnection = {
+    isConnected: 0
+}
+
+export const connect = async() => {
+
+    if (mongoConnection.isConnected) return;
+
+    if (mongoose.connections.length > 0) {
+        mongoConnection.isConnected = mongoose.connections[0].readyState;
+        if (mongoConnection.isConnected === 1) return;
+        await mongoose.disconnect();
+    }
+
+    await mongoose.connect( process.env.MONGO_URL || '');
+    mongoConnection.isConnected = 1;
+    console.log('Conectado a MongoDB:', process.env.MONGO_URL );
+}
+
+export const disconnect = async() => {
+    
+    if (process.env.NODE_ENV === 'development') return;
+
+    if (mongoConnection.isConnected === 0) return;
+
+    // No funciona en Prod
+    return;
+    
+    // await mongoose.disconnect();
+    // mongoConnection.isConnected = 0;
+    // console.log('Desconectado de MongoDB');
+}
