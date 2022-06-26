408f5fb728caf0c60698b3c0972af99be3c5d052
Leandro Arturi <lea.arturi@gmail.com>
2022-06-26 16:18:53 -0300

    Carga inicial Productos

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,20 +1,28 @@
 import type { NextPage } from 'next';
 import { Typography } from '@mui/material';
 import { ShopLayout } from '../components/layouts';
-import { initialData } from '../database/products';
 import { ProductList } from '../components/products';
 
-const Home: NextPage = () => {
+import useSWR from 'swr';
+const fetcher = (...args: [key: string]) => fetch(...args).then(res => res.json());
+
+const HomePage: NextPage = () => {
+
+  const { data, error } = useSWR('api/products', fetcher);
+
+  if (error) return <div>failed to load</div>
+  if (!data) return <div>loading...</div>
+
   return (
     <ShopLayout title={'Teslo-Shop: Home'} pageDescription={'Encuentra los productos que buscas en Teslo-Shop!'}>
       <Typography variant='h1' component='h1'>Home</Typography>
       <Typography variant='h2' sx={{ mb: 1 }}>Todos los productos</Typography>
 
       <ProductList 
-        products={ initialData.products as any } 
+        products={ data as any } 
       />
     </ShopLayout>
   )
 }
 
-export default Home
+export default HomePage
