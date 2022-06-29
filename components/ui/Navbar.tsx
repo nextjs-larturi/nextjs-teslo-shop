23d9b47b500febbc27306b0f2d6502d545c08edf
Leandro Arturi <lea.arturi@gmail.com>
2022-06-28 21:58:59 -0300

    Menu selected

--- components/ui/Navbar.tsx
+++ components/ui/Navbar.tsx
@@ -1,9 +1,15 @@
 import React from 'react';
 import NextLink from 'next/link';
+import { useRouter } from 'next/router';
 import { AppBar, Toolbar, Typography, Link, Box, Button, IconButton, Badge } from '@mui/material';
 import { SearchOutlined, ShoppingCartOutlined } from '@mui/icons-material';
 
 export const Navbar = () => {
+
+  const { asPath } = useRouter();
+
+  const setColorButon = (path: string) => (asPath === path) ? 'primary' : 'info';
+  
   return (
     <AppBar>
         <Toolbar>
@@ -19,17 +25,17 @@ export const Navbar = () => {
             <Box sx={{ display: { xs: 'none', sm: 'block' } }}>
                 <NextLink href='/category/men' passHref>
                     <Link>
-                        <Button>Hombres</Button>
+                        <Button color={setColorButon('/category/men')}>Hombres</Button>
                     </Link>
                 </NextLink>
                 <NextLink href='/category/women' passHref>
                     <Link>
-                        <Button>Mujeres</Button>
+                        <Button color={setColorButon('/category/women')}>Mujeres</Button>
                     </Link>
                 </NextLink>
                 <NextLink href='/category/kid' passHref>
                     <Link>
-                        <Button>Niños</Button>
+                        <Button color={setColorButon('/category/kid')}>Niños</Button>
                     </Link>
                 </NextLink>
             </Box>
