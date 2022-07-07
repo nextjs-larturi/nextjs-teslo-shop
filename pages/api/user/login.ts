a4e05dd0b80fa68bb0623dda425bf8c3eb8bd6cf
Leandro Arturi <lea.arturi@gmail.com>
2022-07-07 10:39:14 -0300

    API Login

---
+++ pages/api/user/login.ts
@@ -0,0 +1,55 @@
+import type { NextApiRequest, NextApiResponse } from 'next';
+import bcrypt from 'bcryptjs';
+import { db } from '../../../database';
+import { User } from '../../../models';
+
+type Data = 
+    | { message: string }
+    | {
+        token: string;
+        user: {
+            email: string;
+            name: string;
+            role: string;
+        }
+    }
+
+export default function handler (req: NextApiRequest, res: NextApiResponse<Data>) {
+    
+    switch (req.method) {
+        case 'POST':
+            return loginUser(req, res);
+    
+        default:
+            return res.status(400).json({ message: 'Bad request' });
+    }
+}
+
+const loginUser = async (req: NextApiRequest, res: NextApiResponse<Data>) => {
+    
+    const { email='', password='' } = req.body;
+
+    await db.connect();
+    const user = await User.findOne({email});
+    await db.disconnect();
+
+    if (!user) {
+        return res.status(400).json({ message: 'User or password incorrect' });
+    }
+
+    if (!bcrypt.compareSync(password, user.password!)) {
+        return res.status(400).json({ message: 'User or password incorrect' });
+    }
+
+    // Login correcto
+    
+    const { role, name } = user;
+
+    return res.status(200).json({
+        token: '',
+        user: {
+            email, role, name
+        }
+    });
+
+}
