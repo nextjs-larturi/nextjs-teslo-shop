6e04633434b8b08911ee36cd7bdec2c0ef33c333
Leandro Arturi <lea.arturi@gmail.com>
2022-07-08 15:42:26 -0300

    Register - Token Validate

--- utils/jwt.ts
+++ utils/jwt.ts
@@ -18,3 +18,24 @@ export const signToken = (_id: string, email: string) => {
     )
 
 }
+
+export const isValidToken = (token: string):Promise<string> => {
+
+    if(!process.env.JWT_SECRET_SEED) {
+        throw new Error("No hay semilla de JWT. Revisar variables de entorno.");
+    }
+
+    return new Promise((resolve, reject) => {
+        try {
+            jwt.verify(token, process.env.JWT_SECRET_SEED || '', (err, payload) => {
+                if (err) return reject('El JWT no es valido');
+
+                const  { _id } = payload as { _id: string };
+
+                resolve(_id);
+            })
+        } catch (error) {
+            reject('El JWT no es valido');
+        }
+    });
+}
