aea516128159dde4c496b1b4bb056a1b40ecdcb2
Leandro Arturi <lea.arturi@gmail.com>
2022-07-08 15:22:31 -0300

    Register - Validacion email

--- pages/api/user/register.ts
+++ pages/api/user/register.ts
@@ -2,7 +2,7 @@ import type { NextApiRequest, NextApiResponse } from 'next';
 import bcrypt from 'bcryptjs';
 import { db } from '../../../database';
 import { User } from '../../../models';
-import { jwt } from '../../../utils';
+import { jwt, validations } from '../../../utils';
 
 type Data = 
     | { message: string }
@@ -41,6 +41,10 @@ const registerUser = async (req: NextApiRequest, res: NextApiResponse<Data>) => 
 
     if (name.length < 6) {
         return res.status(400).json({ message: 'Name must be at least 6 characters'});
+    }
+
+    if(!validations.isValidEmail(email)) {
+        return res.status(400).json({ message: 'El correo no tiene formato valido'});
     }
 
     await db.connect();
