cf18332a067f135d56d38726429024ec869e176c
Leandro Arturi <lea.arturi@gmail.com>
2022-09-03 12:13:22 -0300

    Dashboard Admin Counters

--- interfaces/index.ts
+++ interfaces/index.ts
@@ -1,5 +1,6 @@
-export * from './cart';
-export * from './order';
 export * as IPaypal from './paypal';
+export * from './cart';
+export * from './dashboard';
+export * from './order';
 export * from './products';
 export * from './user';
--- pages/admin/index.tsx
+++ pages/admin/index.tsx
@@ -1,10 +1,46 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import useSWR from 'swr';
 import { DashboardOutlined, CreditCardOffOutlined, AttachmentOutlined, AttachMoneyOutlined, GroupOutlined, CategoryOutlined, CancelPresentation, ProductionQuantityLimitsOutlined, AccessTimeOutlined } from '@mui/icons-material';
-import { Grid } from '@mui/material';
+import { Grid, Typography } from '@mui/material';
 import { AdminLayout } from '../../components/layouts';
 import { SummaryTile } from '../../components/admin';
+import { DashboardSummaryResponse } from '../../interfaces';
 
 const DashboardPage = () => {
+
+  const { data, error } = useSWR<DashboardSummaryResponse>('/api/admin/dashboard', {
+    refreshInterval: 30 * 1000 // 30 seconds
+  });
+
+  const [refreshIn, setRefreshIn] = useState(30);
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setRefreshIn(refreshIn => refreshIn > 0 ? refreshIn - 1 : 30);
+    }, 1000);
+  
+    return () => clearInterval(interval);
+  }, []);
+
+  if( !error && !data ) {
+    return <></>
+  }
+
+  if(error) {
+    console.error(error);
+    return <Typography>Error al cargar la información</Typography>
+  }
+
+  const {
+    numberOfOrders,
+    paidOrders,
+    notPaidOrders,
+    numberOfClients,
+    numberOfProducts,
+    productsWithNoInventory,
+    lowInventory,
+  } = data!;
+
   return (
     <AdminLayout 
         title="Dashboard"
@@ -14,49 +50,49 @@ const DashboardPage = () => {
         <Grid container spacing={2}>
 
           <SummaryTile 
-            title={1}
+            title={numberOfOrders}
             subtitle="Ordenes Totales"
             icon={<CreditCardOffOutlined color="secondary" sx={{ fontSize: 40 }} />}
           />
 
           <SummaryTile 
-            title={2}
+            title={paidOrders}
             subtitle="Ordenes Pagadas"
             icon={<AttachMoneyOutlined color="success" sx={{ fontSize: 40 }} />}
           />
 
           <SummaryTile 
-            title={3}
+            title={notPaidOrders}
             subtitle="Ordenes Pendientes"
             icon={<CreditCardOffOutlined color="error" sx={{ fontSize: 40 }} />}
           />
 
           <SummaryTile 
-            title={4}
+            title={numberOfClients}
             subtitle="Clientes"
             icon={<GroupOutlined color="primary" sx={{ fontSize: 40 }} />}
           />
 
           <SummaryTile 
-            title={5}
+            title={numberOfProducts}
             subtitle="Productos"
             icon={<CategoryOutlined color="warning" sx={{ fontSize: 40 }} />}
           />
 
           <SummaryTile 
-            title={6}
+            title={productsWithNoInventory}
             subtitle="Sin Stock"
             icon={<CancelPresentation color="error" sx={{ fontSize: 40 }} />}
           />
 
           <SummaryTile 
-            title={7}
+            title={lowInventory}
             subtitle="Bajo inventario"
             icon={<ProductionQuantityLimitsOutlined color="warning" sx={{ fontSize: 40 }} />}
           />
 
           <SummaryTile 
-            title={8}
+            title={refreshIn}
             subtitle="Actualización en:"
             icon={<AccessTimeOutlined color="secondary" sx={{ fontSize: 40 }} />}
           />
@@ -69,3 +105,4 @@ const DashboardPage = () => {
 }
 
 export default DashboardPage;
+
